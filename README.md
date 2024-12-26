e0fef48f6ead31224cb2dc0067ad3bd752d40e8e
sch-paulo <pschmitzjr@gmail.com>
2024-12-26 10:38:34 -0400

    ajuste do README e pequenas alterações na pipeline

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # aula09_bootcamp
+Aula sobre funções e explicação inicial de logging
--- pipeline.py
+++ pipeline.py
@@ -3,8 +3,9 @@ from tenacity_decorator import get_user_input
 
 file = 'data'
 output_format = 'parquet and csv'
+
 try:
     get_user_input()
+    pipeline_calculate_kpi_of_total_sales_consolidated(file, output_format)
 except Exception as e:
     print(f"Finalmente falhou após várias tentativas: {e}")
-pipeline_calculate_kpi_of_total_sales_consolidated(file, output_format)
